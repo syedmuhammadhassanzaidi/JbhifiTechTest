c9c5c4abbaa5df105c07df6d0b4be60151299e8b
Hassan Zaidi <hassan.zaidi@purple.telstra.com>
2024-11-26 21:46:11 +1100

    Remaining files for API

---
+++ JbhifiTechTest.Server/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,42 @@
+﻿namespace JbhifiTechTest.Server.Middleware
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string apiKeyHeader;
+        private readonly List<string> _apiKeys;
+
+        public ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            apiKeyHeader = configuration.GetValue<string>("ApiKeyHeader");
+            _apiKeys = configuration.GetSection("ApiKeys").Get<List<string>>();
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(apiKeyHeader, out var apiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("There is no API key provided");
+                return;
+            }
+            if (!_apiKeys.Contains(apiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("The API Key is invalid");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+
+    public static class ApiKeyVerificationMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseApiKeyVerificaion(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ApiKeyMiddleware>();
+        }
+    }
+}
---
+++ JbhifiTechTest.Server/Models/Weather.cs
@@ -0,0 +1,10 @@
+﻿namespace JbhifiTechTest.Server.Models
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+}
